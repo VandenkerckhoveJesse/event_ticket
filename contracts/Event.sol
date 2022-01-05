97bf1307a9226f32f6cbc70086a89dcc176ffad0
jesse.vandenkerckhove <jesse.vandenkerckhove@student.howest.be>
2022-01-05 19:34:07 +0100

    Functions on contracts added, still need to implement them

--- contracts/Event.sol
+++ contracts/Event.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.8.0;
 
 contract Event{
     address public ticketContractAddress;
+
     struct EventInfo{
         string name;
         uint price;
@@ -14,6 +15,18 @@ contract Event{
 
     constructor(address ticketAddress) {
         ticketContractAddress = ticketAddress;
+    }
+
+    function organizeEvent() returns (uint) {
+        return 0;
+    }
+
+    function purchaseTicket(uint eventId) payable returns (uint) {
+        return 0;
+    }
+
+    function redeemTicket(uint ticketId, uint eventId) {
+
     }
 
 }
--- contracts/Ticket.sol
+++ contracts/Ticket.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Ticket is ERC721 {
+    address public eventContractAddress;
+
     struct TicketInfo {
         uint eventId;
         bool redeemed;
@@ -10,6 +12,25 @@ contract Ticket is ERC721 {
     TicketInfo[] public tickets;
 
     constructor() ERC721("Event Ticket", "TICKET") public {
+    }
+
+    function isValid() public view returns (bool){
+        return true;
+    }
+
+    function isRedeemable() public view returns (bool) {
+        return true;
+    }
+
+
+    //todo protect this function, not everyone can just redeem any ticket (the organizer and ticketholder have to both agree)
+    function redeemTicket(uint ticketId, uint eventId) external {
+
+    }
+
+    //again protect this, only the event contract may create new tickets.
+    function createTicket(address buyer, uint eventId) external returns (uint256) {
+        return 1;
     }
 
 
